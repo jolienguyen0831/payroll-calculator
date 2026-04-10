cdd8f9656c34e479041e0edb5398b31722a81c68
jolienguyen0831 <uyennguyen0831@gmail.com>
2026-04-10 10:27:12 -0500

    feat: complete Exercise 4

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class PayrollCalculator {
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String employeeName = input.nextLine();
+        System.out.print("Enter your hours worked: ");
+        float hoursWorked = input.nextFloat();
+        System.out.print("Enter your pay rate: ");
+        float payRate = input.nextFloat();
+
+        float grossPay;
+
+        if (hoursWorked > 40)
+        {
+            float overTime = hoursWorked - 40;
+            float overTimePay = (overTime * 1.5f * payRate);
+            grossPay = (40 * payRate) + overTimePay;
+        }
+        else
+        {
+            grossPay = hoursWorked * payRate;
+        }
+
+        System.out.printf("%s worked %.2f hrs. Gross pay is $%.2f", employeeName, hoursWorked, grossPay);
+
+
+    }
 }
